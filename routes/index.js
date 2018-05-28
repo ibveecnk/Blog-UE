8dc420073205d7809bcf470a88134b379403bf55
dev-ib <iven@b.de>
2018-05-28 11:31:18 +0200

    Database.js erstellt

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
+var db = require('../database.js');
 
-/* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
