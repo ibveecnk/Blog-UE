7f5ea4f2ffb1e0bb03796216da11d095b43bf28b
dev-ib <iven@b.de>
2018-05-28 13:43:08 +0200

    app.js update

--- app.js
+++ app.js
@@ -4,13 +4,12 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var express = require('express');
-var exphbs  = require('express-handlebars');
+var exphbs = require('express-handlebars');
 var mongoose = require('mongoose');
 var Schema = mongoose.Schema,
   ObjectId = Schema.ObjectId;
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
 var app = express();
 
 // view engine setup
@@ -21,31 +20,44 @@ app.engine('hbs', exphbs({
   extname: 'hbs',
   layoutsDir: './views/layouts/'
 }));
+var categorySchema = new Schema({
+  catname: String,
+  caturl: String,
+  _id: Schema.Types.ObjectId
+})
+var authorSchema = new Schema({
+  _id: Schema.Types.ObjectId,
+  Surname: String,
+  Name: String,
+  Since: {
+  type: Date,
+  default: Date.now
+  }
+})
 
-var authorSchema = new Schema(
-  {
-    _id: Schema.Types.ObjectId,
-    Surname: String,
-    Name: String,
-    Since: Date,
-  })
+var postSchema = new Schema({
+  public: Boolean,
+  title: String,
+  content: String,
+  author: ObjectId,
+  date: {
+    type: Date,
+    default: Date.now
+  },
+  category: ObjectId,
+})
 
-var postSchema = new Schema(
-  {
-    public: Boolean,
-    title: String,
-    content: String,
-    author: ObjectId,
-    date: { type: Date, default: Date.now },
-    category: String,
-  })
-  var Author = mongoose.model('Author', authorSchema);
+//author.save(function (err, ) {
+ // if (err) return handleError(err);
+//});
 
 app.set('view engine', 'hbs');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({
+  extended: false
+}));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -53,12 +65,12 @@ app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use(function (req, res, next) {
   next(createError(404));
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use(function (err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
@@ -68,4 +80,7 @@ app.use(function(err, req, res, next) {
   res.render('error');
 });
 
+function handleError(err) {
+console.log(err);
+}
 module.exports = app;
