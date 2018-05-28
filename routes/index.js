a80d0f842a3b5328a44b1f53cbba6f42d0fd5cf4
dev-ib <iven@b.de>
2018-05-28 12:15:48 +0200

    app.js Schema created

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var db = require('../database.js');
+var mongoose = require('mongoose')
 
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
