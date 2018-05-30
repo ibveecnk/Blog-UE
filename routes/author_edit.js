cb83b3c6dff8e34c6e25cc99a7c5bd7c88c95f47
moritzpuchinger <moritz.puchinger@outlook.com>
2018-05-30 16:30:19 +0200

    edit author

--- routes/author_edit.js
+++ routes/author_edit.js
@@ -4,7 +4,7 @@ var app = express();
 var db = require('../database.js')
 
 router.post('/', function (req, res, next) {
-  res.redirect('/addpost')
+  /*res.redirect('/addpost')
   var Author = new db.Author({
     Name: req.body.name,
     Surname: req.body.surname,
@@ -12,7 +12,17 @@ router.post('/', function (req, res, next) {
 
   Author.update({_id: req.body.id}, Author, function (err) {
     if (err) return console.error(err);
-  });
+  });*/
+  db.Author.findById(req.body.id,function(err,author){
+    if (err) return handleError(err);
+
+    author.Surname = req.body.surname;
+    author.Name = req.body.name;
+    author.save(function(err,updatedAuthor){
+      if(err) return handleError(err);
+      res.send(updatedAuthor);
+    })
+  })
 });
 
 module.exports = router;
