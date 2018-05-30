28cdc8402974472b556a061e70611c87b74c5553
dev-ib <iven@b.de>
2018-05-30 13:10:22 +0200

    Login Method

--- routes/do_login.js
+++ routes/do_login.js
@@ -3,7 +3,13 @@ var router = express.Router();
 var db = require('../database.js');
 var mongoose = require('mongoose')
 
-
-
+router.post('/', function (req, res, next) {
+    if(req.body.password === 'qwertzuiopü+'){
+        res.cookie('logged_in', '01e6efdb-9421-4271-83eb-b685f618e2c3');
+        res.send('Success')
+    }   else    {
+        res.redirect('/');
+    }
+  });  
 
 module.exports = router;
