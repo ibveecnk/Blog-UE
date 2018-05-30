591bb4c36401c81abed997eef2b8c85652b98cd6
dev-ib <iven@b.de>
2018-05-30 13:57:06 +0200

    LocalAdminLogout

--- routes/logout.js
+++ routes/logout.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var router = express.Router();
+var app = express();
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
-    res.clearCookie('logged_in');
-    res.render('success', {title: 'Erfolgreich ausgeloggt'})
+    if(req.cookies.logged_in == '01e6efdb-9421-4271-83eb-b685f618e2c3') {
+        app.locals.admin = false;
+        res.clearCookie('logged_in');
+        res.render('success', {title: 'Erfolgreich ausgeloggt'});
+      } else {
+        res.redirect('/');
+      }
+
 });
 
 module.exports = router;
