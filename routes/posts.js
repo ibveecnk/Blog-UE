fb32cc19b9cd6317eb392695481127c7b67c250b
moritzpuchinger <moritz.puchinger@outlook.com>
2018-05-30 10:14:38 +0200

    posts.hbs

--- routes/posts.js
+++ routes/posts.js
@@ -9,8 +9,10 @@ router.all('/:post', function(req, res, next) {
     db.Post.findOne({_id:post},function(err,category){
 
     }).populate("author").lean().exec(function(err,post){
-        post.date = "abc";
-        res.render('posts',{post:JSON.stringify(post)})
+        var dateObj = new Date(post.date);
+        var months = ["Januar","Februar","März","April","Mai","Juni","Juli","August","September","Oktober","November","Dezember"];
+        post.date = ("0"+dateObj.getDate()).slice(-2) + ". " + months[dateObj.getMonth()] + " " + dateObj.getFullYear() + " | " + ("0"+dateObj.getHours()).slice(-2) + ":" + ("0"+dateObj.getMinutes()).slice(-2) + " Uhr";
+        res.render('posts',{post:post})
     })
 })
 
