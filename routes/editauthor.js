f5599403625fa1b91477ba50ccd964eed1de7743
dev-ib <iven@b.de>
2018-06-01 08:44:03 +0200

    Securification of edit author

--- routes/editauthor.js
+++ routes/editauthor.js
@@ -8,15 +8,15 @@ router.get('/:author', function (req, res, next) {
   var authorId = req.params.author;
   if (req.signedCookies.logged_in == '01e6efdb-9421-4271-83eb-b685f618e2c3') {
     app.locals.admin = true;
+    db.Author.findOne({_id:authorId},function(err,result){
+    }).lean().exec(function(err,result){
+      console.log(result.Name);
+      res.render('editauthor', { title: "Autor bearbeiten" ,author:result});
+    })
   } else {
     app.locals.admin = false;
     res.redirect('/login')
   }
-  db.Author.findOne({_id:authorId},function(err,result){
-  }).lean().exec(function(err,result){
-    console.log(result.Name);
-    res.render('editauthor', { title: "Autor bearbeiten" ,author:result});
-  })
 });
 router.get('/', function(req, res, next) {
   db.Author.find({},function(err,result){
