47c71a7c555bba5c7e7835fa253b9e76c3a9a5f6
moritzpuchinger <moritz.puchinger@outlook.com>
2018-06-01 13:04:52 +0200

    check forms, fix submit button style

--- public/scripts/main.js
+++ public/scripts/main.js
@@ -1,1 +1,41 @@
+var lettersandspace = /^[A-Za-z ]+$/;
+var onlyletters = /^[a-zA-Z]+$/;
+var lettersandumlauts = /^[A-Za-z ÄÖÜäöüß]+$/
 $(document).foundation();
+function startAuthorChecker() {
+    setInterval(function(){
+        if(lettersandspace.test(document.querySelector('#surname').value) && lettersandspace.test(document.querySelector('#name').value)) {
+            document.querySelector('#submit').disabled = false;
+        } else {
+            document.querySelector('#submit').disabled = true;
+        }
+    },20)
+}
+function startCategoryChecker() {
+    setInterval(function(){
+        if(lettersandumlauts.test(document.querySelector('#catname').value) && onlyletters.test(document.querySelector('#caturl').value)) {
+            document.querySelector('#submit').disabled = false;
+        } else {
+            document.querySelector('#submit').disabled = true;
+        }
+    },20)
+}
+function startPostChecker() {
+    setInterval(function(){
+        if(lettersandumlauts.test(document.querySelector('#title').value) && document.querySelector('#author').value !== "id" && document.querySelector('#category').value !== 'id' && document.querySelector('#content').value.length > 0) {
+            document.querySelector('#submit').disabled = false;
+        } else {
+            document.querySelector('#submit').disabled = true;
+        }
+    },20)
+}
+var form = document.querySelector('.form');
+if(form != null) {
+    if(form.id == 'authorform') {
+        startAuthorChecker();
+    } else if(form.id == 'categoryform') {
+        startCategoryChecker();
+    } else if(form.id == 'postform') {
+        startPostChecker();
+    }
+}
